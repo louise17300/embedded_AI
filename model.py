8f8aa4ed2d7940fae58780f75dc2b779e7a23ca3
BaertA <arsene.baert@etu.emse.fr>
2022-09-26 11:45:21 +0200

    Somes changes

--- model.py
+++ model.py
@@ -1,8 +1,28 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 """
 Created on Wed Sep 21 11:42:38 2022
 
 @author: arsen
 """
 
-# Séparer la dataset en trois
+# 1. Librairies
+from PIL import Image
+import numpy as np
+from numpy import random
+
+
+import os
+import pathlib
+import random
+
+# 2. Get dataset
+# 2.1. Augment dataset
+
+data
+
+
+# directory of dataset
+dir_original = "/content/drive/MyDrive/augmented_esca_dataset/content/esca_dataset/augmented_esca_dataset"
+
+# name of new dataset
+dir_processed = "/content/drive/MyDrive/augmented_esca_dataset_splited"
